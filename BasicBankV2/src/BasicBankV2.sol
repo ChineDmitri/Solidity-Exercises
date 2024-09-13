8f00e6f553cd65d501ef2829be86b47a0744a176
ChineDmitri <mr.dima11@gmail.com>
2024-09-13 15:13:57 +0200

    Ok... peut-etre

--- BasicBankV2/src/BasicBankV2.sol
+++ BasicBankV2/src/BasicBankV2.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.13;
 
+event EtherRemoved(address indexed user, uint256 amount);
+event EtherRemovalFailed(address indexed user, uint256 amount, string reason);
+
 contract BasicBankV2 {
     /// used to store the balance of users
     ///     USER    =>  BALANCE
@@ -8,9 +11,34 @@ contract BasicBankV2 {
 
     /// @notice deposit ether into the contract
     /// @dev it should work properly when called multiple times
-    function addEther() external payable {}
+    function addEther() external payable {
+        balances[msg.sender] += msg.value;
+    }
 
     /// @notice used to withdraw ether from the contract
     /// @param amount of ether to remove. Cannot execeed balance i.e users cannot withdraw more than they deposited
-    function removeEther(uint256 amount) external payable {}
+    function removeEther(uint256 amount) external payable {
+
+        if (amount == 0) {
+            emit EtherRemovalFailed(msg.sender, amount, "Le montant doit etre superieur a zero");
+        }   
+
+        if (amount > balances[msg.sender]) {
+            emit EtherRemovalFailed(msg.sender, amount, "Solde insuffisant");
+        }        
+        
+    	payable(msg.sender).transfer(amount);
+    }
+
+    function getBalancesContract() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getBalances() public view returns (uint256) {
+        return balances[msg.sender];
+    }
+
+    function getBalanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }       
 }
--- BasicBankV2/test/BasicBankV2.t.sol
+++ BasicBankV2/test/BasicBankV2.t.sol
@@ -23,15 +23,23 @@ contract BasicBankV2Test is Test {
 
     function testRemoveEther() external {
         vm.deal(address(this), 1 ether);
+        console.log("-- basicBankV2.balances(this) BEFORE addEther ", basicBankV2.getBalancesContract());
         basicBankV2.addEther{value: 1 ether}();
+        console.log("-- basicBankV2.balances(this) AFTER addEther ", basicBankV2.getBalancesContract());
+
+        console.log("-- basicBankV2.balances(address(basicBankV2))", basicBankV2.balances(address(basicBankV2)));
+        console.log("-- basicBankV2.balances(this)", basicBankV2.balances(address(this)));
+
 
         vm.expectRevert();
         basicBankV2.removeEther(2 ether);
+        console.log("-- basicBankV2.balances(this) AFTER addtrying remove 2 ether ", basicBankV2.getBalancesContract());
 
-        basicBankV2.removeEther(1 ether);
+        basicBankV2.removeEther(0.5 ether);
+        console.log("-- basicBankV2.balances(this) AFTER addtrying remove 1 ether ", basicBankV2.getBalancesContract());
         assertEq(
             address(this).balance,
-            1 ether,
+            0.5 ether,
             "expected balance of address(this) to be 1 ether"
         );
     }
