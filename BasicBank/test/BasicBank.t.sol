43c58e4db23400f730b700cf4f60928df1c6da21
ChineDmitri <mr.dima11@gmail.com>
2024-09-13 11:02:05 +0200

    Correcction OK + add logs in test

--- BasicBank/test/BasicBank.t.sol
+++ BasicBank/test/BasicBank.t.sol
@@ -14,19 +14,26 @@ contract BasicBankTest is Test {
     function testAddEther() external {
         vm.deal(address(this), 1 ether);
         basicBank.addEther{value: 1 ether}();
+        console.log(" -- address(basicBank).balance BEFORE : ", address(basicBank).balance);
+        console.log(" -- msg.sender", msg.sender);
         assertEq(
             address(basicBank).balance,
             1 ether,
             "expected balance of basic bank contract to be 1 ether"
         );
+        console.log(" -- address(basicBank).balance AFTER : ", address(basicBank).balance);
     }
 
     function testRemoveEther() external {
         vm.deal(address(this), 1 ether);
         vm.expectRevert();
         basicBank.removeEther(1);
+        console.log(" -- address(this).balance BEFORE ADD ", address(basicBank).balance); 
         basicBank.addEther{value: 1 ether}();
+        console.log(" -- address(this).balance AFTER ADD ", address(basicBank).balance); 
         basicBank.removeEther(1 ether);
+        console.log(" -- address(this).balance AFTER REMOVE ", address(basicBank).balance);
+        console.log(" -- msg.sender", msg.sender);
         assertEq(
             address(this).balance,
             1 ether,
